54136613d2605f62b821062a1ae6524fe8d3bf70
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-02-07 15:39:39 +0600

    first server

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.get('/',(req, res) =>{
+    res.send('<h1>Hello World!<h1>')
+})
+
+app.get('/posts',(req,res) =>{
+    res.send('<h1> I am post page.<h1>')
+})
+
 app.listen(PORT, () => {
     console.log(`server is running on PORT ${PORT}`)
 })
