a63de793e0cb0954f5a21c9de223a04b397bfe9a
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-02-07 16:01:06 +0600

    first server post

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ app.get('/posts',(req,res) =>{
     res.send('<h1> I am post page.<h1>')
 })
 
+app.get('/api/contents',(req,res) =>{
+    res.json(contacts)
+})
+
+app.post('/api/contents',(req,res) => {
+    res.json('I am post method')
+})
+
 app.listen(PORT, () => {
     console.log(`server is running on PORT ${PORT}`)
 })
+
+const contacts = [
+    {name: 'Sabbir', email: 'example1@gmail.com'},
+    {name: 'Hosen', email: 'example2@gmail.com'},
+    {name: 'Asif', email: 'example3@gmail.com'}
+]
