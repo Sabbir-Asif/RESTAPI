6aa486ec41e3ffe9a9bb1baed1fb3cafa7fa0715
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-02-08 14:15:28 +0600

    after adding all routes

--- api/routs/contact.js
+++ api/routs/contact.js
@@ -9,4 +9,31 @@ router.get('/',(req,res,next) =>{
     )
 })
 
+// Post
+router.post('/',(req,res,next) =>{
+    res.status(201).json(
+        {message: 'Hello, I am all contacts Post Route'}
+    )
+})
+
+router.get('/:id', (req,res,next) =>{
+    id = req.params.id
+    res.json({
+        id
+    })
+})
+
+router.put('/',(req,res,next) =>{
+    res.status(201).json(
+        {message: 'Hello, I am put Route'}
+    )
+})
+
+router.delete('/',(req,res,next) =>{
+    res.status(201).json(
+        {message: 'Hello, I am delete Route'}
+    )
+})
+
+
 module.exports = router;
